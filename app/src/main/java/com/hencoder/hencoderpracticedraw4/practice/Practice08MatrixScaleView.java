4f3decd46c4bab4ae225282750d35030f859f63c
TANXIN <602273157@qq.com>
2017-08-22 22:18:58 +0800

    Initial commit

---
+++ app/src/main/java/com/hencoder/hencoderpracticedraw4/practice/Practice08MatrixScaleView.java
@@ -0,0 +1,53 @@
+package com.hencoder.hencoderpracticedraw4.practice;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Matrix;
+import android.graphics.Paint;
+import android.graphics.Point;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.View;
+
+import com.hencoder.hencoderpracticedraw4.R;
+
+public class Practice08MatrixScaleView extends View {
+    Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
+    Bitmap bitmap;
+    Point point1 = new Point(200, 200);
+    Point point2 = new Point(600, 200);
+    Matrix matrix = new Matrix();
+
+    public Practice08MatrixScaleView(Context context) {
+        super(context);
+    }
+
+    public Practice08MatrixScaleView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public Practice08MatrixScaleView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    {
+        bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.maps);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        canvas.save();
+        matrix.setScale(1.3f,1.3f,bitmap.getWidth()/2+point1.x,bitmap.getHeight()/2+point1.y);
+        canvas.concat(matrix);
+        canvas.drawBitmap(bitmap, point1.x, point1.y, paint);
+        canvas.restore();
+
+        canvas.save();
+        matrix.setScale(0.6f,1.6f,bitmap.getWidth()/2+point2.x,bitmap.getHeight()/2+point2.y);
+        canvas.concat(matrix);
+        canvas.drawBitmap(bitmap, point2.x, point2.y, paint);
+    }
+}
